d5907d1f90d5da998f7a6aad14d9cb1ff657df92
ntkhoa95 <toankhoabk@gmail.com>
2023-09-08 13:50:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
+# python main.py --data /mnt/data4/taindp/WORKSPACE/04_COMPET/classifier/dataset_phase2_decouple \
+#   --dataset mosquito \
+#   -b 8 \
+#   --use_norm True \
+#   --lr 0.001 -p 200 --epochs 100 \
+#   --arch convnext_base.fb_in22k_ft_in1k \
+#   --wd 1e-4 --cos False \
+#   --cl_views sim-sim
+
 python main.py --data /mnt/data4/taindp/WORKSPACE/04_COMPET/classifier/dataset_phase2_decouple \
   --dataset mosquito \
-  -b 8 \
+  -b 4 \
   --use_norm True \
   --lr 0.001 -p 200 --epochs 100 \
-  --arch resnet50 \
+  --arch maxvit_base_tf_224.in21k \
   --wd 1e-4 --cos False \
   --cl_views sim-sim
