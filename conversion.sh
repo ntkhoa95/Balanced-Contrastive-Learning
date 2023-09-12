825ea61716f3940999fab5b73d5c3c8830b840d8
ntkhoa95 <toankhoabk@gmail.com>
2023-09-12 10:22:39 +0700

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -1,4 +1,4 @@
-python conversion.py --arch maxvit_base_tf_224.in21k \
+python conversion.py --arch resnext50 \
     --checkpoint_dir ./log/mosquito_resnext50_batchsize_8_epochs_100_temp_0.07_lr_0.001_sim-sim/bcl_ckpt.best.pth.tar \
     --image_size 224 \
     --num_classes 6
