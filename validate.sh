c83b0333ff75f59f94f6405d96a5b495dc25ceb8
ntkhoa95 <toankhoabk@gmail.com>
2023-09-12 16:28:23 +0700

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -5,4 +5,5 @@ python validate.py --data /mnt/data4/taindp/WORKSPACE/04_COMPET/classifier/data/
   --use_norm True \
   --arch resnext50 \
   --cl_views sim-sim \
-  --resume log/mosquito_resnext50_batchsize_8_epochs_100_temp_0.07_lr_0.001_sim-sim/bcl_ckpt.best.pth.tar
+  --resume log/mosquito_resnext50_batchsize_8_epochs_100_temp_0.07_lr_0.001_sim-sim/bcl_ckpt.best.pth.tar \
+  --reload True
