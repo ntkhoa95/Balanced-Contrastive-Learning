fe3886994a752818533461b07abfc85dfbf3b02b
ntkhoa95 <toankhoabk@gmail.com>
2023-09-13 15:58:06 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,16 @@ import random
 from PIL import ImageFilter
 import numpy as np
 import torch
-
+from sklearn.metrics import f1_score
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    classification_report,
+    precision_recall_fscore_support,
+    accuracy_score
+)
 
 ## freezing
 def count_parameters(model):
@@ -76,3 +85,48 @@ def shot_acc(preds, labels, train_data, many_shot_thr=100, low_shot_thr=20, acc_
     else:
         class_accs = 0
         return np.mean(many_shot), np.mean(median_shot), np.mean(low_shot), class_accs
+
+
+def F_measure(preds, labels, openset=False, theta=None):
+    
+    if openset:
+        # f1 score for openset evaluation
+        true_pos = 0.
+        false_pos = 0.
+        false_neg = 0.
+        
+        for i in range(len(labels)):
+            true_pos += 1 if preds[i] == labels[i] and labels[i] != -1 else 0
+            false_pos += 1 if preds[i] != labels[i] and labels[i] != -1 else 0
+            false_neg += 1 if preds[i] != labels[i] and labels[i] == -1 else 0
+
+        precision = true_pos / (true_pos + false_pos)
+        recall = true_pos / (true_pos + false_neg)
+        return 2 * ((precision * recall) / (precision + recall + 1e-12))
+    else:
+        # Regular f1 score
+        return f1_score(labels.detach().cpu().numpy(), preds.detach().cpu().numpy(), average='macro') * 100
+
+def mic_acc_cal(preds, labels):
+    # Micro accuracy
+    acc_mic_top1 = (preds == labels).sum().item() / len(labels)
+    return acc_mic_top1 * 100
+
+def calculate_metrics(pred, target, target_names):
+    res = []
+    cls_report = classification_report(
+        y_true=np.array(target),
+        y_pred=np.array(pred),
+        target_names=target_names,
+    )
+
+    return {
+        'report': cls_report,
+        'accuracy': accuracy_score(target, pred),
+        'micro/precision': precision_score(y_true=target, y_pred=pred, average='micro'),
+        'micro/recall': recall_score(y_true=target, y_pred=pred, average='micro'),
+        'micro/f1': f1_score(y_true=target, y_pred=pred, average='micro'),
+        'macro/precision': precision_score(y_true=target, y_pred=pred, average='macro'),
+        'macro/recall': recall_score(y_true=target, y_pred=pred, average='macro'),
+        'macro/f1': f1_score(y_true=target, y_pred=pred, average='macro'),
+    }
